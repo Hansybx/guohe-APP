cc20f22cff6f1fef00f7f98fdd6ed378d8d7e85f
hansybx <1579450419@qq.com>
2020-03-06 21:04:39 +0800

    版本与分享按钮

--- lib/routes/myPage/myPage.dart
+++ lib/routes/myPage/myPage.dart
@@ -1,9 +1,6 @@
-import 'package:dio/dio.dart';
 import 'package:flustars/flustars.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_app/common/constUrl.dart';
 import 'package:flutter_app/common/localShare.dart';
-import 'package:flutter_app/routes/myPage/one.dart';
 import 'package:flutter_icons/flutter_icons.dart';
 import 'package:share/share.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -58,8 +55,14 @@ class _MyPageState extends State<MyPage> {
               ListTile(
                 title: new Text("版本"),
                 subtitle: Text(version),
-                onTap: ()=>Share.share('check out my website https://example.com', subject: 'Look what I made!'),
-                leading: Icon(AntDesign.form),
+                leading: Icon(AntDesign.info),
+                trailing: Icon(Icons.keyboard_arrow_right),
+              ),
+
+              ListTile(
+                title: new Text("分享"),
+                onTap: ()=>Share.share('test',subject: 'Look what I made!'),
+                leading: Icon(AntDesign.sharealt),
                 trailing: Icon(Icons.keyboard_arrow_right),
               ),
 
